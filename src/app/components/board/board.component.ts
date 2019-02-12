62a9a749eed4dcad4a30436973a89702428ada3b
nosteiner <no.steiner@gmail.com>
2019-02-12 23:32:08 +0200

    set new positions according to window resize

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { RulesService } from 'src/app/services/rules.service';
 import { TurnsService } from 'src/app/services/turns.service';
 
@@ -7,26 +7,82 @@ import { TurnsService } from 'src/app/services/turns.service';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit, AfterViewInit {
 
   constructor(private rulesService: RulesService, private turnsService: TurnsService) { }
 
+  event: MouseEvent;
   matrix;
+
+  columnsPositions = [];
 
   ngOnInit() {
     this.matrix = this.rulesService.matrix;
   }
 
-  handleClickOnCol(colId) {
-    const colIndex = this.getColNumberById(colId);
+  ngAfterViewInit() {
+    this.setColumnsCoordinates();
+  }
+
+  handleClickOnCol(event) {
+    const colIndex = this.columnsPositions.indexOf(this.findColumnByPositionX(event.clientX));
     if (this.rulesService.isColInsertable(colIndex)) {
       this.rulesService.insertTokenToCol(colIndex);
       this.turnsService.switchTurns();
     }
   }
 
-  getColNumberById(colId) {
-    return Number(colId.replace(/^\D+/g, ''));
+  setColumnsCoordinates() {
+    for (let i = 0; i < 7; i++) {
+      const colId = 'col' + i;
+      const columnElement = document.getElementById(`${colId}`);
+      const colPosition = this.getColumnPosition(columnElement);
+      if (this.columnsPositions.length > 6) {
+        this.columnsPositions[i] = colPosition;
+      } else {
+        this.columnsPositions.push(colPosition);
+      }
+    }
   }
 
+  getColumnPosition(el) {
+    let xPos = 0;
+    let yPos = 0;
+
+    while (el) {
+      if (el.tagName === 'BODY') {
+        // deal with browser quirks with body/window/document and page scroll
+        const xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+        const yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+        xPos += (el.offsetLeft - xScroll + el.clientLeft);
+        yPos += (el.offsetTop - yScroll + el.clientTop);
+      } else {
+        // for all other non-BODY elements
+        xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+        yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+      }
+
+      el = el.offsetParent;
+    }
+    return {
+      x: xPos,
+      y: yPos
+    };
+  }
+
+  findColumnByPositionX(mouseUpPositionX) {
+    const colwidth = 72;
+
+    return this.columnsPositions.find((element) => {
+      const elementStartX = element.x;
+      const elementEndX = element.x + colwidth;
+
+      return (elementStartX <= mouseUpPositionX && mouseUpPositionX < elementEndX);
+    });
+  }
+
+  onResize(event) {
+  this.setColumnsCoordinates();
+  }
 }
