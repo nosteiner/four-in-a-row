e1477dead7e4f166943871743cb62d972acc499a
nosteiner <no.steiner@gmail.com>
2019-02-12 18:35:06 +0200

    set start screen

--- src/app/Cell.ts
+++ src/app/Cell.ts
@@ -5,8 +5,8 @@ export class Cell {
 
     constructor() {
         this.isEmpty = true;
-        this.player = new Player(0, this.color);
-        this.color = new Color(230, 230, 230);
+        this.player = null;
+        this.color = new Color(255, 255, 255);
     }
 
     isEmpty: Boolean;
